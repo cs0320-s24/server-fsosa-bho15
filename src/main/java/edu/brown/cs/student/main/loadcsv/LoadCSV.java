058017c86851890b8469e175909f426da2b30b28
Brendan Ho <brendanho@brendansair343.devices.brown.edu>
2024-02-10 12:06:27 -0500

    feat: added classes for API queries, LoadCSV, SearchCSV, ViewCSV

---
+++ src/main/java/edu/brown/cs/student/main/loadcsv/LoadCSV.java
@@ -0,0 +1,6 @@
+package edu.brown.cs.student.main.loadcsv;
+
+public class LoadCSV {
+  private String filePath;
+  public LoadCSV() {}
+}
---
+++ src/main/java/edu/brown/cs/student/main/viewcsv/ViewCSV.java
@@ -0,0 +1,5 @@
+package edu.brown.cs.student.main.viewcsv;
+
+public class ViewCSV {
+  public ViewCSV() {}
+}
