f32c15dc611d4fe7d08cc626e12909798e35242e
Brendan Ho <brendanho@brendansair343.devices.brown.edu>
2024-02-13 17:47:55 -0500

    feat: working load/search/view

--- src/main/java/edu/brown/cs/student/main/census/Census.java
+++
@@ -1,7 +0,0 @@
-package edu.brown.cs.student.main.census;
-
-public class Census {
-  private String state;
-  private String county;
-  private String S2802_C03_022E;
-}
--- src/main/java/edu/brown/cs/student/main/viewcsv/ViewCSV.java
+++
@@ -1,5 +0,0 @@
-package edu.brown.cs.student.main.viewcsv;
-
-public class ViewCSV {
-  public ViewCSV() {}
-}
