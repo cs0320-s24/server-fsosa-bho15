be2043d7b36db8d6575d5f38bfeb1cfe23998248
Brendan Ho <brendanho@brendansair343.mynetworksettings.com>
2024-02-13 23:16:51 -0500

    bug: deserializing json from api issue

--- src/main/java/edu/brown/cs/student/main/census/CensusAPIUtilities.java
+++ src/main/java/edu/brown/cs/student/main/census/CensusAPIUtilities.java
@@ -3,15 +3,16 @@ package edu.brown.cs.student.main.census;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import java.io.IOException;
+import okio.Buffer;
 
 public class CensusAPIUtilities {
-  public static Census deserializeCensus(String jsonCensus) {
+  public static Census deserializeCensus(Buffer jsonCensus) {
     try {
       // Initializes Moshi
       Moshi moshi = new Moshi.Builder().build();
 
       // Initializes an adapter to a Census class then uses it to parse the JSON.
-      JsonAdapter<Census> adapter = moshi.adapter(Census.class);
+      JsonAdapter<Census> adapter = moshi.adapter(Census.class).nonNull();
       return adapter.fromJson(jsonCensus);
     } catch (IOException e) {
       e.printStackTrace();
--- src/main/java/edu/brown/cs/student/main/server/CensusHandler.java
+++ src/main/java/edu/brown/cs/student/main/server/CensusHandler.java
@@ -1,15 +1,23 @@
 package edu.brown.cs.student.main.server;
 
+import static spark.Spark.connect;
+
 import edu.brown.cs.student.main.census.CensusAPIUtilities;
+import edu.brown.cs.student.main.exceptions.DataSourceException;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
+import okio.Buffer;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -37,10 +45,19 @@ public class CensusHandler implements Route {
     Map<String, Object> responseMap = new HashMap<>();
     try {
       // Sends a request to the API and receives JSON back
-      String censusJson = this.sendRequest(stateCode, countyCode);
+      URL url =
+          new URL(
+              "https://api.census.gov/data/2021/acs/acs1/subject/variables?"
+                  + "get=NAME,S2802_C03_022E&for=county:"
+                  + countyCode
+                  + "&in=state:"
+                  + stateCode);
+      HttpURLConnection censusJson = connect(url);
       // Adds results to the responseMap
       responseMap.put("result", "success");
-      responseMap.put("census", CensusAPIUtilities.deserializeCensus(censusJson));
+      responseMap.put(
+          "census",
+          CensusAPIUtilities.deserializeCensus(new Buffer().readFrom(censusJson.getInputStream())));
       return responseMap;
     } catch (Exception e) {
       e.printStackTrace();
@@ -52,7 +69,23 @@ public class CensusHandler implements Route {
     return responseMap;
   }
 
-  private String sendRequest(String stateCode, String countyCode)
+  /**
+   * Private helper method; throws IOException so different callers can handle differently if
+   * needed.
+   */
+  private static HttpURLConnection connect(URL requestURL) throws DataSourceException, IOException {
+    URLConnection urlConnection = requestURL.openConnection();
+    if (!(urlConnection instanceof HttpURLConnection))
+      throw new DataSourceException("unexpected: result of connection wasn't HTTP");
+    HttpURLConnection clientConnection = (HttpURLConnection) urlConnection;
+    clientConnection.connect(); // GET
+    if (clientConnection.getResponseCode() != 200)
+      throw new DataSourceException(
+          "unexpected: API connection not success status " + clientConnection.getResponseMessage());
+    return clientConnection;
+  }
+
+  private HttpResponse<String> sendRequest(String stateCode, String countyCode)
       throws URISyntaxException, IOException, InterruptedException {
     HttpRequest buildCensusApiRequest =
         HttpRequest.newBuilder()
@@ -78,7 +111,7 @@ public class CensusHandler implements Route {
     System.out.println(sentCensusApiResponse.body());
 
     //    return sentCensusApiResponse.body();
-    return null;
+    return sentCensusApiResponse;
   }
 
   private String requestStateCode(String stateName)
